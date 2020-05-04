2c6d3a234430765c804504c01d71d503ae5df850
leoniedarois <leonie.grimoin@gmail.com>
2020-05-05 01:10:40 +0200

    first illustration

---
+++ assets/js/anim.js
@@ -0,0 +1,125 @@
+// PARTICULES PART
+/* particlesJS.load(@dom-id, @path-json */
+particlesJS('particles-js', {
+    "particles": {
+        "number": {
+            "value": 70,
+            "density": {
+                "enable": true,
+                "value_area": 600
+            }
+        },
+        "color": {
+            "value": "#ffffff"
+        },
+        "shape": {
+            "type": "circle",
+            "stroke": {
+                "width": 0,
+                "color": "#000000"
+            },
+            "polygon": {
+                "nb_sides": 5
+            }
+        },
+        "opacity": {
+            "value": 0.7,
+            "random": false,
+            "anim": {
+                "enable": false,
+                "speed": 1,
+                "opacity_min": 0.1,
+                "sync": false
+            }
+        },
+        "size": {
+            "value": 3,
+            "random": true,
+            "anim": {
+                "enable": false,
+                "speed": 40,
+                "size_min": 0.1,
+                "sync": false
+            }
+        },
+        "line_linked": {
+            "enable": false,
+            "distance": 150,
+            "color": "#ffffff",
+            "opacity": 0.4,
+            "width": 1
+        },
+        "move": {
+            "enable": true,
+            "speed": 4,
+            "direction": "none",
+            "random": false,
+            "straight": false,
+            "out_mode": "out",
+            "bounce": false,
+            "attract": {
+                "enable": false,
+                "rotateX": 600,
+                "rotateY": 1200
+            }
+        }
+    },
+    "interactivity": {
+        "detect_on": "canvas",
+        "events": {
+            "onhover": {
+                "enable": true,
+                "mode": "repulse"
+            },
+            "onclick": {
+                "enable": true,
+                "mode": "push"
+            },
+            "resize": true
+        },
+        "modes": {
+            "grab": {
+                "distance": 400,
+                "line_linked": {
+                    "opacity": 1
+                }
+            },
+            "bubble": {
+                "distance": 400,
+                "size": 40,
+                "duration": 2,
+                "opacity": 8,
+                "speed": 3
+            },
+            "repulse": {
+                "distance": 50,
+                "duration": 0.4
+            },
+            "push": {
+                "particles_nb": 4
+            },
+            "remove": {
+                "particles_nb": 2
+            }
+        }
+    },
+    "retina_detect": true
+});
+
+// PARALLAX PART
+let scene = document.getElementById('scene');
+let parallaxInstance = new Parallax(scene);
+
+// GSAP PART
+TweenLite.defaultEase = Linear.easeNone;
+
+let rideau = new TimelineMax({repeat: -1, yoyo: true});
+rideau.to("#RIDEAU", .5, { y: 0, x: -1});
+
+let lights = new TimelineMax({repeat: -1, yoyo: true});
+lights.to("#LUMIERES", 2, { opacity: 0.8 });
+lights.to("#LUMIERES", 2, { y: 2, x: -3 });
+
+let test = new TimelineMax({repeat: -1, yoyo: true});
+test.to("#bras_mec", .5, { y: 0, x: -1});
+
